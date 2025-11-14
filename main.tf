7776faad87c4cfaada55e2d5dd83824c6796fd19
Prajwal-Gitlabhub <prajwal.jogi4241@gmail.com>
2025-11-15 00:12:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
+resource "aws_instance" "wipro" {
 
+    ami = "ami-001843b876406202a"
+    instance_type = "t2.micro"
+    tags = {
+        "Name" = "wipro5"
+        "ttl" ="1300"
+        "owner" = "wipro"
+    }
+}
+
+resource "aws_s3_bucket" "d1" {
+  bucket = "14112025day"
+}
+
+terraform {
+  cloud {
+    organization = "cumminstrainer"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for Terraform Cloud
+    hostname = "app.terraform.io"
+
+    workspaces {
+      tags = ["terraformwipro"]
+    }
+  }
+}
